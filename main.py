5a5e0b35310f6ca89704eb1faf886703471d7400
Pedro Barros <pedrokatsbarros@gmail.com>
2020-11-22 05:54:40 -0300

    Criadas funções para inserção de tupla na tabela fator

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
 import pymysql #biblioteca baixada para manipulação de BDs MySQL
 
 def main():
-    conexao = establishConnectionDB()
+    DBConnection = establishConnectionDB()
     try:
         showMainMenu()
         option = returnMainMenuOption()
-        handlesSelectedMainMenuOption(option)
-        cursor = conexao.cursor()
+        handlesSelectedMainMenuOption(option, DBConnection)
         
     finally: #Todo o código do bloco finally  será executado, caso tenha ocorrido uma exceção ou não
-        conexao.close()
+        DBConnection.close()
     
 def establishConnectionDB():
     conexao = pymysql.connect(db='UFJF_Modelo_Futebol', user='Pedro Barros', passwd='712Ax2+712bx+c=0')
@@ -28,8 +27,9 @@ def returnMainMenuOption():
     option = input('Enter an option number: ')
     return option
 
-def handlesSelectedMainMenuOption(option):
+def handlesSelectedMainMenuOption(option, DBConnection):
     if (option == '1'):
+        InsertTupleTableFator(DBConnection)
         return
     if (option == '2'):
         return
@@ -40,10 +40,53 @@ def handlesSelectedMainMenuOption(option):
 
     showErrorMessage(1)
 
+def getsAttributeNameNewTupleTableFator():
+    name = input('Enter the factor name (required): ')
+    if (name == ''):
+        showErrorMessage(2)
+        return None
+    return name
+
+def getsAttributeStatusNewTupleTableFator():
+    status = input('Enter the factor status (1 for true or anything for false): ')
+    if (status == '1'):
+        statusBool = True
+    else:
+        statusBool = False
+
+    return statusBool
+
+def getsAttributeTypeNewTupleTableFator():
+    factorType = input('Enter the factor type (required): ')
+    if (factorType == ''):
+        showErrorMessage(3)
+        return None
+    return factorType
+
+def InsertTupleTableFator(DBConnection):
+    name = ''
+    status = False
+    factorType = ''
+    name = getsAttributeNameNewTupleTableFator()
+    if (name == None):
+        return
+    status = getsAttributeStatusNewTupleTableFator()
+    factorType = getsAttributeTypeNewTupleTableFator()
+    if (factorType == None):
+        return
+    cursor = DBConnection.cursor()
+    cursor.execute("INSERT INTO fator(nome, status, tipo) VALUES ('" + name + "', " + str(status) + ", '" + factorType + "')")
+    DBConnection.commit()
+        
+
 def showErrorMessage(msgCode):
     errMsg = '__________________ ERROR __________________\n'
     if (msgCode == 1):
         errMsg += 'Invalid option.\n'
+    if (msgCode == 2):
+        errMsg += 'The factor name is required.\n'
+    if (msgCode == 3):
+        errMsg += 'The factor type is required.\n'
 
     errMsg += '___________________________________________\n'
     print(errMsg)
