04b34d54c7d0106ea6f14f6a71dc48ff28e31a61
Pedro Barros <pedrokatsbarros@gmail.com>
2020-11-22 23:59:41 -0300

    Criadas funções para inserção de peso no banco de dados

--- main.py
+++ main.py
@@ -32,6 +32,7 @@ def handlesSelectedMainMenuOption(option, DBConnection):
         InsertTupleTableFator(DBConnection)
         return
     if (option == '2'):
+        InsertTupleTablePeso(DBConnection)
         return
     if (option == '3'):
         return
@@ -49,7 +50,7 @@ def getsAttributeNameNewTupleTableFator():
         showErrorMessage(4)
         return None
 
-    return name
+    return name.upper()
 
 def getsAttributeStatusNewTupleTableFator():
     status = input('Enter the factor status (1 for \'true\' or anything for \'false\'): ')
@@ -67,7 +68,7 @@ def getsAttributeTypeNewTupleTableFator():
     else:
         factorType = 'Externo'  
     
-    return factorType
+    return factorType.upper()
 
 def InsertTupleTableFator(DBConnection):
     name = ''
@@ -82,7 +83,93 @@ def InsertTupleTableFator(DBConnection):
     cursor.execute("INSERT INTO fator(nome, status, tipo) VALUES ('" + name + "', " + str(status) + ", '" + factorType + "')")
     DBConnection.commit()
     showOkMessage(1)
+
+def InsertTupleTablePeso(DBConnection):
+    name = ''
+    factorName = ''
+    valor = 0.00
+
+    if (not IsThereFactor(DBConnection)):
+        showErrorMessage(3)
+        return
+    name = getsAttributeNameNewTupleTablePeso(DBConnection)
+    if (name == None):
+        return
+    factorName = getsAttributeFactorNameNewTupleTablePeso(DBConnection)
+    if (factorName == None):
+        return
+    valor = getsAttributeValueNewTupleTablePeso()
+    if (valor == None):
+        return
+
+    cursor = DBConnection.cursor()
+    cursor.execute("INSERT INTO peso(nome, nomefator, valor) VALUES ('" + name + "', '" + factorName + "', " + str(valor) + ")")
+    DBConnection.commit()
+    showOkMessage(2)
+
+def getsAttributeNameNewTupleTablePeso(DBConnection):
+    name = input('Enter the weight name (required): ')
+    if (name == ''):
+        showErrorMessage(5)
+        return None
+    if (len(name) > 60):
+        showErrorMessage(6)
+        return None
+    if (isAnExistingWeight(DBConnection, name)):
+        showErrorMessage(11)
+        return None
+    
+    
+    return name.upper()
+
+def getsAttributeFactorNameNewTupleTablePeso(DBConnection):
+    factorName = input('Enter the name of the factor to which the weight will belong (required): ')
+    if (factorName == '' or len(factorName) > 45):
+        showErrorMessage(7)
+        return None
+    if (not isAnExistingFactor(DBConnection, factorName)):
+        showErrorMessage(8)
+        return None
+
+    return factorName.upper()
+
+def isAnExistingFactor(DBConnection, factorName):
+    cursor = DBConnection.cursor()
+    cursor.execute("SELECT NOME FROM FATOR\
+                    WHERE NOME = '" + factorName + "'")
+    queryResult = cursor.fetchone()
+    return queryResult != None
+
+def isAnExistingWeight(DBConnection, weightName):
+    cursor = DBConnection.cursor()
+    cursor.execute("SELECT NOME FROM PESO\
+                    WHERE NOME = '" + weightName + "'")
+    queryResult = cursor.fetchone()
+    return queryResult != None
         
+
+def getsAttributeValueNewTupleTablePeso():
+    strValue = input('Enter the value of weight (required): ')
+    floatValue = float(strValue)
+    if (not isNumberInRange(floatValue, 0.00, 1.00)):
+        showErrorMessage(9)
+        return None
+    
+    return floatValue
+        
+
+def isNumberInRange(number, min, max):
+    if (min > max):
+        return false
+    
+    return number >= min and number <= max
+    
+
+def IsThereFactor(DBConnection):
+    cursor = DBConnection.cursor()
+    cursor.execute("SELECT COUNT(NOME) From FATOR")
+    queryResult = cursor.fetchone()
+    return queryResult[0] > 0
 
 def showErrorMessage(msgCode):
     errMsg = '__________________ ERROR __________________\n'
@@ -90,8 +177,22 @@ def showErrorMessage(msgCode):
         errMsg += 'Invalid option.\n'
     if (msgCode == 2):
         errMsg += 'The factor name is required.\n'
+    if (msgCode == 3):
+        errMsg += 'There is not at least one factor registered.\n'
     if (msgCode == 4):
         errMsg += 'The factor name has exceeded 45 characters.\n'
+    if (msgCode == 5):
+        errMsg += 'The weight name is required.\n'
+    if (msgCode == 6):
+        errMsg += 'The weight name has exceeded 60 characters.\n'
+    if (msgCode == 7):
+        errMsg += 'Name of the factor to which the weight will belong is invalid.\n'
+    if (msgCode == 8):
+        errMsg += 'This factor does not correspond to any registered factor.\n'
+    if (msgCode == 9):
+        errMsg += 'The weight value is invalid.\n'
+    if (msgCode == 11):
+        errMsg += 'The name of the new weight already exists.\n'
 
     errMsg += '___________________________________________\n'
     print(errMsg)
@@ -100,6 +201,8 @@ def showOkMessage(msgCode):
     okMsg = '__________________ OK! __________________\n'
     if (msgCode == 1):
         okMsg += 'Factor inserted successfully!\n'
+    if (msgCode == 2):
+        okMsg += 'Weight inserted successfully!\n'
     
     okMsg += '_________________________________________\n'
     print(okMsg)
