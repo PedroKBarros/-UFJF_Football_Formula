ace4418c8b0bbd4cbdc7ce0de9a0482294ab095e
Pedro Barros <pedrokatsbarros@gmail.com>
2020-11-23 01:42:32 -0300

    Adicionadas opções de apresentação de fatores, pesos e sair, além de alteração na interface gráfica do menu principal. Além disso, inserção da pasta __pycache__ no .gitignore

--- main.py
+++ main.py
@@ -7,10 +7,17 @@ MAIN_MENU_NUM_OPTION1 = '1'
 MAIN_MENU_NUM_OPTION2 = '2'
 MAIN_MENU_NUM_OPTION3 = '3'
 MAIN_MENU_NUM_OPTION4 = '4'
+MAIN_MENU_NUM_OPTION5 = '5'
+MAIN_MENU_NUM_OPTION6 = '6'
+MAIN_MENU_NUM_OPTION7 = '7'
 MAIN_MENU_STR_OPTION1 = 'Inserir Fator'
 MAIN_MENU_STR_OPTION2 = 'Inserir Peso'
 MAIN_MENU_STR_OPTION3 = 'Alterar Status de Fator'
-MAIN_MENU_STR_OPTION4 = 'Criar Simulação'
+MAIN_MENU_STR_OPTION4 = 'Apresentar Fatores'
+MAIN_MENU_STR_OPTION5 = 'Apresentar Pesos'
+MAIN_MENU_STR_OPTION6 = 'Criar Simulação'
+MAIN_MENU_STR_OPTION7 = 'Sair'
+
 MAIN_MENU_TITLE = 'MAIN MENU'
 
 #Other Constants
@@ -28,10 +35,13 @@ def main():
 
 def showMainMenu():
     menu = '_____________________ ' + MAIN_MENU_TITLE + ' ____________________\n'
-    menu += '|' + MAIN_MENU_NUM_OPTION1 + '.' + MAIN_MENU_STR_OPTION1 + '                                   |\n'
-    menu += '|' + MAIN_MENU_NUM_OPTION2 + '.' + MAIN_MENU_STR_OPTION2 + '                                    |\n'
-    menu += '|' + MAIN_MENU_NUM_OPTION3 + '.' + MAIN_MENU_STR_OPTION3 + '                         |\n'
-    menu += '|' + MAIN_MENU_NUM_OPTION4 + '.' + MAIN_MENU_STR_OPTION4 + '                                 |\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION1 + '.' + MAIN_MENU_STR_OPTION1 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION2 + '.' + MAIN_MENU_STR_OPTION2 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION3 + '.' + MAIN_MENU_STR_OPTION3 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION4 + '.' + MAIN_MENU_STR_OPTION4 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION5 + '.' + MAIN_MENU_STR_OPTION5 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION6 + '.' + MAIN_MENU_STR_OPTION6 + '\n'
+    menu += ' ' + MAIN_MENU_NUM_OPTION7 + '.' + MAIN_MENU_STR_OPTION7 + '\n'             
     menu += '___________________________________________________\n'
     print(menu)
 
