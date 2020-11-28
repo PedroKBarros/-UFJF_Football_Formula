67f9a4fb6939618c5a0b7565b21c641543d12785
Pedro Barros <pedrokatsbarros@gmail.com>
2020-11-28 01:59:16 -0300

    Colocadas o restante das strings literais do módulo sql_functions como constantes declaradas no inicio do módulo. As únicas constantes literais que se manterão dessa forma são as querys do DB

--- sql_functions.py
+++ sql_functions.py
@@ -6,23 +6,39 @@ DB_NAME = 'UFJF_Modelo_Futebol'
 DB_USER = 'Pedro Barros'
 DB_PASSWORD = '712Ax2+712bx+c=0'
 
-#Data request phrases constants
-PHR_NAME_NEW_TUPLE_TABLE_FATOR = 'Enter the factor name (required): '
-PHR_STATUS_NEW_TUPLE_TABLE_FATOR = 'Enter the factor status (1 for \'true\' or anything for \'false\'): '
-PHR_TYPE_NEW_TUPLE_TABLE_FATOR = 'Enter the factor type (1 for \'Interno\' or anything for \'externo\'): '
-PHR_NAME_NEW_TUPLE_TABLE_PESO = 'Enter the weight name (required): '
-PHR_FACTOR_NEW_TUPLE_TABLE_PESO = 'Enter the name of the factor to which the weight will belong (required): '
-PHR_VALUE_NEW_TUPLE_TABLE_PESO = 'Enter the value of weight (required): '
-PHR_OPTION_PRESENTATION_FACTORS = 'Show factor names only (\'y\' for \'yes\' or anything to show all informations)? '
-PHR_OPTION_PRESENTATION_WEIGHTS = 'Show weight names only (\'y\' for \'yes\' or anything to show all informations)? '
-PHR_NAME_CHANGE_FACTOR_STATUS = 'Enter the name of factor: '
-PHR_STATUS_CHANGE_FACTOR_STATUS = 'Enter the factor status (1 for \'true\' or anything for \'false\'): '
-
 #constants values
 VAL_INTERNO_TYPE_FATOR = 'Interno'
 VAL_EXTERNO_TYPE_FATOR = 'Externo'
 VAL_ON_STATUS_FATOR = 'On'
 VAL_OFF_STATUS_FATOR = 'Off'
+VAL_1_AS_TRUE = '1'
+VAL_Y_AS_YES = 'Y'
+VAL_REQUIRED = 'required'
+
+#Data request phrases constants
+PHR_NAME_NEW_TUPLE_TABLE_FATOR = 'Enter the factor name (required): '
+PHR_STATUS_NEW_TUPLE_TABLE_FATOR = 'Enter the factor status (' + VAL_1_AS_TRUE + ' for \'true\' or anything for \'false\'): '
+PHR_TYPE_NEW_TUPLE_TABLE_FATOR = 'Enter the factor type (' + VAL_1_AS_TRUE + ' for \'' + VAL_INTERNO_TYPE_FATOR + '\'' + ' or anything for \'' + VAL_EXTERNO_TYPE_FATOR + '\'): '
+PHR_NAME_NEW_TUPLE_TABLE_PESO = 'Enter the weight name (' + VAL_REQUIRED + '): '
+PHR_FACTOR_NEW_TUPLE_TABLE_PESO = 'Enter the name of the factor to which the weight will belong ('+ VAL_REQUIRED + '): '
+PHR_VALUE_NEW_TUPLE_TABLE_PESO = 'Enter the value of weight (' + VAL_REQUIRED + '): '
+PHR_OPTION_PRESENTATION_FACTORS = 'Show factor names only (\'' + VAL_Y_AS_YES + '\' for \'yes\' or anything to show all informations)? '
+PHR_OPTION_PRESENTATION_WEIGHTS = 'Show weight names only (\'' + VAL_Y_AS_YES + '\' for \'yes\' or anything to show all informations)? '
+PHR_NAME_CHANGE_FACTOR_STATUS = 'Enter the name of factor: '
+PHR_STATUS_CHANGE_FACTOR_STATUS = 'Enter the factor status (' + VAL_1_AS_TRUE + ' for \'true\' or anything for \'false\'): '
+
+#Constant output data phrases
+PHROUT_HEADER_PRESENT_ALL_FACTORS = '\n__________________ FACTORS __________________'
+PHROUT_TOTAL_PRESENT_ALL_FACTORS_WEIGHTS = 'Total: '
+PHROUT_FOOTER_PRESENT_ALL_FACTORS = "_____________________________________________"
+PHROUT_STATUS_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_FACTORS = "    Status: "
+PHROUT_TIPO_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_FACTORS = "    Tipo: "
+PHROUT_ITEM_CHARACTER = ">"
+PHROUT_HEADER_PRESENT_ALL_WEIGHTS = '__________________ WEIGHTS __________________'
+PHROUT_FOOTER_PRESENT_ALL_WEIGHTS = "_____________________________________________"
+PHROUT_FACTOR_NAME_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_WEIGHTS = "    Factor Name: "
+PHROUT_VALUE_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_WEIGHTS = "    Value: "
+PHROUT_CURRENT_VALUE_CHANGE_FACTOR_STATUS = 'Current Status: '
 
 #constants ANSI code colors
 ANSI_DEFAULT_FINAL_COLOR_CODE = '\033[0;0m'
@@ -63,7 +79,7 @@ def getsAttributeNameNewTupleTableFator(DBConnection):
 
 def getsAttributeStatusNewTupleTableFator():
     status = input(PHR_STATUS_NEW_TUPLE_TABLE_FATOR)
-    if (status == '1'):
+    if (status == VAL_1_AS_TRUE):
         statusBool = True
     else:
         statusBool = False
@@ -72,7 +88,7 @@ def getsAttributeStatusNewTupleTableFator():
 
 def getsAttributeTypeNewTupleTableFator():
     factorType = input(PHR_TYPE_NEW_TUPLE_TABLE_FATOR)
-    if (factorType == '1'):
+    if (factorType == VAL_1_AS_TRUE):
         factorType = VAL_INTERNO_TYPE_FATOR
     else:
         factorType = VAL_EXTERNO_TYPE_FATOR 
@@ -171,7 +187,7 @@ def presentsAllFactors(DBConnection):
     option = getsOptionPresentationFactors()
     cursor = DBConnection.cursor()
     optionUpper = option.upper()
-    if (optionUpper == 'Y'):
+    if (optionUpper == VAL_Y_AS_YES):
         cursor.execute('SELECT NOME FROM FATOR')
         isShowOnlyName = True
     else:
@@ -180,10 +196,10 @@ def presentsAllFactors(DBConnection):
     
     queryResult = cursor.fetchall()
     formattedResult = formatResultQueryTableFactor(queryResult, isShowOnlyName)
-    print("\n__________________ FACTORS __________________")
-    print('Total: ' + str(len(queryResult)) + '\n')
+    print('\n' + PHROUT_HEADER_PRESENT_ALL_FACTORS)
+    print(PHROUT_TOTAL_PRESENT_ALL_FACTORS_WEIGHTS + str(len(queryResult)) + '\n')
     print(formattedResult)
-    print("_____________________________________________")
+    print(PHROUT_FOOTER_PRESENT_ALL_FACTORS)
 
 
 def formatResultQueryTableFactor(queryResult, isShowOnlyName):
@@ -197,7 +213,7 @@ def formatResultQueryTableFactor(queryResult, isShowOnlyName):
 def formatResultQueryTableFactorShowingName(queryResult):
     formattedResult = ''
     for result in queryResult:
-        formattedResult += ">" + result[0] + '\n'
+        formattedResult += PHROUT_ITEM_CHARACTER + result[0] + '\n'
     
     return formattedResult
 
@@ -205,14 +221,14 @@ def formatResultQueryTableFactorShowingAllAttributes(queryResult):
     formattedResult = ''
     status = ''
     for result in queryResult:
-        formattedResult += ">" + result[0] + '\n'
+        formattedResult += PHROUT_ITEM_CHARACTER + result[0] + '\n'
         status = bool(result[1])
         if (status):
             statusStr = ANSI_GREEN_INITIAL_CODE + VAL_ON_STATUS_FATOR + ANSI_DEFAULT_FINAL_COLOR_CODE
         else:
             statusStr = ANSI_RED_INITIAL_CODE + VAL_OFF_STATUS_FATOR + ANSI_DEFAULT_FINAL_COLOR_CODE
-        formattedResult += "    Status: " + statusStr + '\n'
-        formattedResult += "    Tipo: " + result[2] + '\n'
+        formattedResult += PHROUT_STATUS_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_FACTORS + statusStr + '\n'
+        formattedResult += PHROUT_TIPO_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_FACTORS + result[2] + '\n'
 
     return formattedResult
 
@@ -228,7 +244,7 @@ def presentsAllWeights(DBConnection):
     option = getsOptionPresentationWeights()
     cursor = DBConnection.cursor()
     optionUpper = option.upper()
-    if (optionUpper == 'Y'):
+    if (optionUpper == VAL_Y_AS_YES):
         cursor.execute('SELECT NOME FROM PESO')
         isShowOnlyName = True
     else:
@@ -237,10 +253,10 @@ def presentsAllWeights(DBConnection):
     
     queryResult = cursor.fetchall()
     formattedResult = formatResultQueryTableWeight(queryResult, isShowOnlyName)
-    print("\n__________________ WEIGHTS __________________")
-    print('Total: ' + str(len(queryResult)) + '\n')
+    print('\n' + PHROUT_HEADER_PRESENT_ALL_WEIGHTS)
+    print(PHROUT_TOTAL_PRESENT_ALL_FACTORS_WEIGHTS + str(len(queryResult)) + '\n')
     print(formattedResult)
-    print("_____________________________________________")
+    print(PHROUT_FOOTER_PRESENT_ALL_WEIGHTS)
 
 def getsOptionPresentationWeights():
     option = input(PHR_OPTION_PRESENTATION_WEIGHTS)
@@ -257,17 +273,16 @@ def formatResultQueryTableWeight(queryResult, isShowOnlyName):
 def formatResultQueryTableWeightShowingName(queryResult):
     formattedResult = ''
     for result in queryResult:
-        formattedResult += ">" + result[0] + '\n'
+        formattedResult += PHROUT_ITEM_CHARACTER + result[0] + '\n'
     
     return formattedResult
 
 def formatResultQueryTableWeightShowingAllAttributes(queryResult):    
     formattedResult = ''
     for result in queryResult:
-        formattedResult += ">" + result[0] + '\n'
-        formattedResult += "    nome: " + result[0] + '\n'
-        formattedResult += "    nome do fator: " + result[1] + '\n'
-        formattedResult += "    valor: " + str(result[2]) + '\n'
+        formattedResult += PHROUT_ITEM_CHARACTER + result[0] + '\n'
+        formattedResult += PHROUT_FACTOR_NAME_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_WEIGHTS + result[1] + '\n'
+        formattedResult += PHROUT_VALUE_FORMAT_SHOWING_ALL_ATTRIBUTES_PRESENT_ALL_WEIGHTS + str(result[2]) + '\n'
 
     return formattedResult
 
@@ -285,7 +300,7 @@ def changeFactorStatus(DBConnection):
     else:
         statusStr = ANSI_RED_INITIAL_CODE + VAL_OFF_STATUS_FATOR + ANSI_DEFAULT_FINAL_COLOR_CODE
 
-    print('Current Status: ' + statusStr)
+    print(PHROUT_CURRENT_VALUE_CHANGE_FACTOR_STATUS + statusStr)
     newStatus = getsNewStatusChangeStatus()
     newStatusStr = str(newStatus)
     if (status == newStatus):
@@ -311,7 +326,7 @@ def getsFactorNameChangeStatus(DBConnection):
 
 def getsNewStatusChangeStatus():
     newStatus = input(PHR_STATUS_CHANGE_FACTOR_STATUS)
-    if (newStatus == '1'):
+    if (newStatus == VAL_1_AS_TRUE):
         statusBool = True
     else:
         statusBool = False
